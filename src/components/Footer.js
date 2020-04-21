bdef4b1e26b040edae4f0c4077d6920eb2660963
Adam Collier <adam@adamcollier.me>
2020-04-21 14:47:27 +0100

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,11 @@ import {
 	Link,
 } from "react-router-dom";
 
-console.log(process.env);
-
 class Footer extends React.Component {
+
 	render() {
+		let currentRepo = ( process.env.NODE_ENV === "production" ) ? `${process.env.REACT_APP_NOW_GITHUB_REPO}/${process.env.NOW_GITHUB_COMMIT_REF}` : "DEV VERSION";
+		let currentRepoURL = (process.env.NODE_ENV === "production" ) ? `https://www.github.com/nxswap/${process.env.REACT_APP_NOW_GITHUB_REPO}` : "https://www.github.com/nxswap/nxswap-taker";
 		return (
 			<div id="footer">
 				<div className="max-width">
@@ -14,13 +15,15 @@ class Footer extends React.Component {
 						<Link to="/about">About NXSwap</Link>
 						<span>|</span>
 						<Link to="/market-makers">Market Makers</Link>
+						<span>|</span>
+						<Link to="/support">Get Support</Link>
 					</div>
 					<div className="right">
-						<Link to="/support">Get Support</Link>
-						<span>|</span>
 						<Link to="/security">Security</Link>
 						<span>|</span>
 						<a href="https://www.github.com/nxswap/nxswap-taker" target="_blank" rel="noopener noreferrer">Open Source</a>
+						<span>|</span>
+						<a href={currentRepoURL}>{currentRepo}</a>
 					</div>
 				</div>
 			</div>
